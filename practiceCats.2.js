0019f1bc7fd2370fac3f70d23515f27cbab46a0e
Anson Allseitz <aallseitz@saintmarys.edu>
2022-11-09 19:02:22 -0500

    added GetDateCreated paramater

--- practiceCats.2.js
+++ practiceCats.2.js
@@ -1,32 +1,33 @@
 function listFolderContents() {
-    var foldername = 'Final Logos'; // provide the name of Folder from which you want to get the list of files
-    var ListOfFiles = 'ListOfFiles_' + foldername;
-  
-    var ss = SpreadsheetApp.create(ListOfFiles);
-    var sheet = ss.getActiveSheet();
-    sheet.appendRow( ['name', 'link','sizeInMB'] );
-  
-    function addFolder(folder) {
-      var contents = folder.getFiles();
-  
-      while(contents.hasNext()) {
-        var var_file = contents.next();
-        var var_name = var_file.getName();
-        var var_link = var_file.getUrl();
-        var var_size=var_file.getSize()/1024.0/1024.0;
-        sheet.appendRow( [var_name, var_link,var_size] );
-      }
-  
-      var subfolders = folder.getFolders();
-  
-      while (subfolders.hasNext()) {
-        var subfolder = subfolders.next();
-        addFolder(subfolder);
-      }
+  var foldername = 'Final Logos'; // provide the name of Folder from which you want to get the list of files
+  var ListOfFiles = 'ListOfFiles_' + foldername;
+
+  var ss = SpreadsheetApp.create(ListOfFiles);
+  var sheet = ss.getActiveSheet();
+  sheet.appendRow( ['name', 'link','sizeInMB', 'DateCreated'] );
+
+  function addFolder(folder) {
+    var contents = folder.getFiles();
+
+    while(contents.hasNext()) {
+      var var_file = contents.next();
+      var var_name = var_file.getName();
+      var var_link = var_file.getUrl();
+      var var_size=var_file.getSize()/1024.0/1024.0;
+      var var_createDate = var_file.getDateCreated(); 
+      sheet.appendRow( [var_name, var_link,var_size, var_createDate] );
     }
-  
-    var folders = DriveApp.getFoldersByName(foldername)
-    var folder = folders.next();
-  
-    addFolder(folder);
-  };
+
+    var subfolders = folder.getFolders();
+
+    while (subfolders.hasNext()) {
+      var subfolder = subfolders.next();
+      addFolder(subfolder);
+    }
+  }
+
+  var folders = DriveApp.getFoldersByName(foldername)
+  var folder = folders.next();
+
+  addFolder(folder);
+};
